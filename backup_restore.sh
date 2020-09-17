bb3cfc7ba964f1f3d72f94e78f80626ead2ce1de
Colin <colin.fonta@gmail.com>
2020-09-17 10:31:05 +0200

    Fixed bugs and errors in the backup_restore.sh

--- backup_restore.sh
+++ backup_restore.sh
@@ -9,36 +9,54 @@
 # - Delete all files  (can be set with the delete_files variable)
 
 user="root"
-current_date=`date +%F_%H%M%S`
-db_number="1 2 3"
 delete_files="true"
 
-for i in $db_numbers;do
+for i in 1 2 3;do
+  # Set variables
+  current_date=`date +%F_%H%M%S`
+  dump_name="${i}_${current_date}"
+
+  # Print info
+  printf "db104_$i process\n"
+
   # Recreate databases (this code is commented in the sql file)
+  printf "Recreate databases $i\n"
   mysql -u $user -e "DROP DATABASE IF EXISTS db104_$i;"
   mysql -u $user -e "CREATE DATABASE IF NOT EXISTS db104_$i;"
   
   # Create sql files (copy template)
+  printf "Create sql files $i\n"
   cp ./databases/db104_template.sql ./databases/db104_$i.sql
 
   # Insert data into databases
-  mysql -u $user -p db104_$i < ./databases/db104_$i.sql
+  printf "Insert data into databases    "
+  mysql -u $user db104_$i < ./databases/db104_$i.sql
+  printf "OK \n"
   
   # Dump databases
-  mysqldump -u $user -p db104_$i > ./databases/dumps/dump_db104_$i_$current_date.sql
+  printf "Dump databases    "
+  mysqldump -u $user db104_$i > ./databases/dumps/dump_db104_$dump_name.sql
+  printf "OK \n"
   
-  # Recreate dbs (already in the dump file)
-  mysql -u $user -e "CREATE DATABASE db104_$i;"
+  # Recreate dbs (this code is commented in the sql dumps)
+  printf "Recreate dbs\n"
   mysql -u $user -e "DROP DATABASE db104_$i;"
+  mysql -u $user -e "CREATE DATABASE db104_$i;"
   
   ### Restore databases
-  mysql -u $user -p db104_$i < dump_db104_$i_$current_date.sql
+  printf "Restore databases   "
+  mysql -u $user db104_$i < ./databases/dumps/dump_db104_$dump_name.sql
+  printf "OK \n"
+
+  # Print info
+  printf "\n"
 done
 
 # Delete all the files
-if [delete_files -eq "true"];then
-  for i in $db_numbers;do
-    rm ./databases/db104_$i.sql
-    rm ./databases/dumps/dump_db104_$i_$current_date.sql
+if [ $delete_files == "true" ];then
+  for i in 1 2 3;do
+   printf "Remove files db\n"
+   rm ./databases/db104_$i.sql
   done
+  rm ./databases/dumps/*
 fi
